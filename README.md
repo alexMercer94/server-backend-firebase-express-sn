8bdff426599b77c9fdd906e8b323ec544e2bc474
Anzen Digital <alex.c.valera94@hotmail.com>
2019-07-30 17:53:14 -0500

    Basic structure

---
+++ README.md
@@ -0,0 +1,9 @@
+# Backed Sever with Express and Firebase
+
+## Command
+
+> To deploy functions and upload the endpoints to firebase use:
+
+`````
+firebase deploy
+`````
---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is the screm body',
+            createdAt: '2019-07-30T22:37:42.256Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+};
