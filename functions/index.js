ffba3577c41d6dfecfaac386ebc57b7df06f0a0b
Anzen Digital <alex.c.valera94@hotmail.com>
2019-08-14 17:32:16 -0500

    Like & unlike functions added

--- functions/index.js
+++ functions/index.js
@@ -31,8 +31,8 @@ app.get('/screams', getAllScreams);
 app.post('/scream', FBAuth, postOneScream);
 app.get('/scream/:screamId', getScream);
 // app.delete('/scream/:screamId', FBAuth, deleteScream);
-// app.get('/scream/:screamId/like', FBAuth, likeScream);
-// app.get('/scream/:screamId/unlike', FBAuth, unlikeScream);
+app.get('/scream/:screamId/like', FBAuth, likeScream);
+app.get('/scream/:screamId/unlike', FBAuth, unlikeScream);
 // app.post('/scream/:screamId/comment', FBAuth, commentOnScream);
 
 // users routes
